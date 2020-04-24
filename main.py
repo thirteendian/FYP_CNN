546c5d5575b0215892efa6ce2583fa4ed1e518ea
Yuxuan <yyx980325@hotmail.com>
2020-04-24 03:17:41 +0100

    update

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
-########################################################################################################################
+############################################################
 # Main
-# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------
 # This file is the main()
-########################################################################################################################
-
+############################################################
+############################################################
 from argparse import ArgumentParser
 from preprocessing import data_access
 from evaluation import evaluate
 from cnn import train
 
 
-
-#define the ArgumentParser type for split spring of training/evaluation data
+# define the ArgumentParser type for split spring of
+# training/evaluation data
 def int_range(string):
-    string1,string2=string.split(':')
-    return range(int(string1),int(string2))
+    string1, string2 = string.split(':')
+    return range(int(string1), int(string2))
+
 
 def main():
-    parser=ArgumentParser(description='CNN ONSET DETECTION BY YUXUAN YANG')
+    parser = ArgumentParser(description=
+                    'CNN ONSET DETECTION BY YUXUAN YANG')
     parser.add_argument(
         '--epochs',
-        type = int,
+        type=int,
         default=100,
         help='select training epochs'
     )
-    fun_select=parser.add_mutually_exclusive_group(required=True)
+    fun_select = \
+        parser.add_mutually_exclusive_group(required=True)
     fun_select.add_argument(
         '--train',
         type=int_range,
@@ -33,19 +36,24 @@ def main():
     fun_select.add_argument(
         '--evaluate',
         type=int_range,
-        help= 'select evaluation models'
+        help='select evaluation models'
     )
-    arg=parser.parse_args()
+    arg = parser.parse_args()
 
-    #import processed data
+    # import processed data
+    # data type:
+    # data[8] [files] [(AudioSample)]
+    #
     data = data_access()
-    print('CREATED DATASET. SIZE %s.'%list(map(len,data)))
 
-    #select function:
+    print(
+    'CREATED DATASET WITH SIZE %s.' % list(map(len, data)))
+
+    # select function:
     if arg.evaluate:
-        evaluate(data,arg.evaluate)
+        evaluate(data, arg.evaluate)
     else:
-        train(data,arg.train,arg.epochs)
+        train(data, arg.train, arg.epochs)
 
 
 if __name__ == '__main__':
